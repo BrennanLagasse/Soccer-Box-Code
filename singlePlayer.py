720ef203f7e2f243fe5c10de3da9b7b8494b5a4b
Brennan Lagasse <51427169+BrennanLagasse@users.noreply.github.com>
2021-06-25 10:45:37 -0400

    Rewrote code for readability, fixed indentation

--- singlePlayer.py
+++ singlePlayer.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python3
-# rpi_ws281x library strandtest example
-# Author: Tony DiCola (tony@tonydicola.com)
-#
-# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
-# various animations on a strip of NeoPixels.
+# Single Player Mode
 
-# Consider lights starting in middle
-# 1 player where trigger stops lights and continues series
-# 2 player with above rules, 2 distinct colors, in parallel
-# 2 player with above rules, 2 distinct colors, not in parallel
+# 1 player game
+# Targets light up for a set amount of time
+# If the target is hit before time expires, add a point to the score and go to the next target
+# If time expires, go to the next target
+# Targets are chosen in random order
+# Run for a set duration of time
+
+
 
 
 import time
@@ -27,27 +26,28 @@ LED_BRIGHTNESS = 100     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
+# Other Constants
+LED_PER_TARGET = 33
+NUM_TARGERTS = 8
+
 
 
 # Define functions which animate LEDs in various ways.
 def colorWipe(strip, color, wait_ms, target):
     """Wipe color across display a pixel at a time."""
-    # for i in range(strip.numPixels()):
-    start = 1+((target-1)*33)
-    for i in range(start-1,start+32):
+    start = (target*LED_PER_TARGET)
+
+    for i in range(start, start + LED_PER_TARGET):
         strip.setPixelColor(i, color)
         strip.show()
-# put ball hit check here and use "break" to end loop
         time.sleep(wait_ms/1000.0)
-		
 	
-def Fillall(strip, color, target):
-   # for i in range(strip.numPixels()):
-   start = 1+((target-1)*33)
-   for i in range (start-1, start+32):
+def fillAll(strip, color, target):
+    """Instantly change color of pixels in target range"""
+    start = 1+(target*LED_PER_TARGET)
+    for i in range (start-1, start+32):
         strip.setPixelColor(i, color)
-   strip.show()
-   # time.sleep(0.25)
+    strip.show()
 
 # Main program logic follows:
 if __name__ == '__main__':
@@ -67,37 +67,44 @@ if __name__ == '__main__':
         print('Use "-c" argument to clear LEDs on exit')
         args = parser.parse_args()
 
-    # Create NeoPixel object with appropriate configuration.
+    # Create NeoPixel object
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-    # Intialize the library (must be called once before other functions).
+
+    # Intialize NeoPixel
     strip.begin()
 
+    # Termination Condition
     print ('Press Ctrl-C to quit.')
     if not args.clear:
         print('Use "-c" argument to clear LEDs on exit')
-
+    
+    # User Input
     wait_ms = input("Time in seconds to hit target:\n")
-    wait_ms = int(wait_ms) / 33*1000
+    wait_ms = int(wait_ms) / LED_PER_TARGET*1000 
     length = input("Time in seconds of game:\n")
     length = int(length)
     start_time = time.time()
-    try:
 
+
+    try:
         while True:
-            # print ('Color wipe animations.')
-            # clearall(strip, Color(255, 0, 0))
-            target = randint(1, 8)
+            # Pick random target and color
+            target = randint(0, NUM_TARGERTS - 1)
             pickcolor = randint(1, 3)
-            # print (pickcolor)
-            # colorWipe(strip, Color(0, 0, 0), wait_ms)  # Red wipe
+
+            # Color Lights based on target
             if pickcolor == 1:
-              Fillall(strip, Color(255, 0, 0), target)
+                fillAll(strip, Color(255, 0, 0), target)
             if pickcolor == 2:
-              Fillall(strip, Color(0, 255, 0), target)
+                fillAll(strip, Color(0, 255, 0), target)
             if pickcolor == 3:
-                Fillall(strip, Color(0, 0, 255), target)
-                colorWipe(strip, Color(0, 0, 0), wait_ms, target)
-                current_time = time.time()
+                fillAll(strip, Color(0, 0, 255), target)
+            
+            # Reset light color
+            colorWipe(strip, Color(0, 0, 0), wait_ms, target)
+
+            # Check if time has expired and terminate if it has
+            current_time = time.time()
             if current_time-start_time > length:
                break
 
