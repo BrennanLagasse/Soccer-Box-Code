1428d82363d986566597660feee0b2afb11c2bf5
Brennan Lagasse <51427169+BrennanLagasse@users.noreply.github.com>
2021-08-27 12:15:29 -0400

    Piezoceramics (6) and Serial Updates

--- piezo_event_test.ino
+++ piezo_event_test.ino
@@ -1,8 +1,9 @@
-const int numPiezos = 3;
-const int piezoPin [numPiezos] = {A0, A1, A2};
-const int ledPin [numPiezos] = {2, 4, 7};
-int piezoVal [numPiezos] = {0, 0, 0};
-int triggerTime [numPiezos] = {0, 0, 0};
+const int numPiezos = 6;
+const int piezoPin [numPiezos] = {A0, A1, A2, A3, A4, A5};
+const int ledPin [numPiezos] = {2, 4, 7, 8, 12, 13};
+int piezoVal [numPiezos] = {0, 0, 0, 0, 0, 0};
+int triggerTime [numPiezos] = {0, 0, 0, 0, 0, 0};
+bool piezosTriggered [numPiezos] = {false, false, false, false, false, false};
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,13 +13,14 @@ void setup() {
   for(int i = 0; i < numPiezos; i++) {
     pinMode(ledPin[i], OUTPUT);
   }
-  
+
+  // Ensure serial frequency is the same for the pi
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  const int threshold = 100;
+  const int threshold = 75;
   const int bounce = 100;
   int m = millis();
 
@@ -28,14 +30,41 @@ void loop() {
 
     // Return if passes threshold
     if(piezoVal[i] > threshold) {
-      Serial.println(i);
+      // Set state to triggered and send on message
+      if(piezosTriggered[i] == false) {
+        sendData(i, true);
+        piezosTriggered[i] = true;
+      }
+
+      // Update timer
       triggerTime[i] = m;
+
+      // Turn on LED
       digitalWrite(ledPin[i], HIGH);  
     }
-    else if(m - triggerTime[i] > bounce) {
+    else if(piezosTriggered[i] && m - triggerTime[i] > bounce) {
+      // Message pi that the piezo is no longer triggered
+      sendData(i, false);
+
+      // Turn off LED
       digitalWrite(ledPin[i], LOW);
+
+      // Set state to not triggered
+      piezosTriggered[i] = false;
     }
     
   }
   
+}
+
+void sendData(int pin, bool triggered) {
+  Serial.print(pin);
+  Serial.print(" ");
+
+  if(triggered) {
+    Serial.println("ON");
+  }
+  else {
+    Serial.println("OFF");
+  }
 }
