457f629a19086bfae927db683ab4b5754e43fc42
Brennan Lagasse <51427169+BrennanLagasse@users.noreply.github.com>
2022-06-29 19:16:48 -0400

    revert to remove score

--- Raspberry Pi Code/sb_goat.py
+++ Raspberry Pi Code/sb_goat.py
@@ -1,22 +1,9 @@
 # Most recent code as of 11/19/2021
 
 import time
-
-# OLED Display
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
-
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
-import subprocess
-
 import serial
-
 from random import random
 from random import randint
-
 import argparse
 
 # Lights
@@ -32,13 +19,6 @@ LED_INVERT     = False
 LED_CHANNEL    = 0
 LED_PER_TARGET = 33
 NUM_TARGETS = 8
-
-# OLED info
-RST = None     # on the PiOLED this pin isnt used
-# Note the following are only used with SPI:
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
 
 # Colors
 RED = Color(255, 0, 0)
@@ -88,38 +68,6 @@ prev = []
 # Tnitialize and start RGB lights
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 strip.begin()
-
-# Initialize OLED (128 x 32 with I2C)
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-disp.begin()
-
-# Clear OLED.
-disp.clear()
-disp.display()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
-
-
-# Load default font.
-font = ImageFont.load_default()
 
 
 # Define functions which animate LEDs in various ways.
@@ -415,25 +363,6 @@ if __name__ == '__main__':
             try:
                 while True:
                     game.update()
-
-                    # Draw a black filled box to clear the image.
-                    draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-                    # Change score (currently arbitrary for testing)
-                    score = [10, 7]
-
-                    # Write two lines of text.
-
-                    draw.text((x, top),       "P1",  font=font, fill=255)
-                    draw.text((x, top+8),     str(score[0]), font=font, fill=255)
-
-                    draw.text((x + 50, top),       "P2",  font=font, fill=255)
-                    draw.text((x + 50, top+8),     str(score[1]), font=font, fill=255)
-
-                    # Display image.
-                    disp.image(image)
-                    disp.display()
-                    time.sleep(.1)
                     
                     if (game.exit):
                         print("GAME OVER\n")
