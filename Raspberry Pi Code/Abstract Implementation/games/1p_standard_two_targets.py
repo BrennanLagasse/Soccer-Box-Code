5071b3db73f3640997b7ec1787eba2c91d043475
Brennan Lagasse <brennan.lagasse@gmail.com>
2022-08-14 12:29:51 -0400

    new games and updates

--- Raspberry Pi Code/Abstract Implementation/games/1p_standard_two_targets.py
+++ Raspberry Pi Code/Abstract Implementation/games/1p_standard_two_targets.py
@@ -5,16 +5,34 @@ from game_manager import GameManager
 NUM_PLAYERS = 1
 
 class StandardOnePlayerGame(GameManager):
-    """Standard game except player can choose from either of two targets (NOT DONE)"""
+    """Standard game except player can choose from either of two targets (Still need to check on second target)"""
     def __init__(self):
         super().__init__(NUM_PLAYERS)
 
         # Pick initial targets
         for room in range(0, len(self._games)):
             self._games[room][0].setTarget(super().pickRandomTarget(room))
+            self._games[room][0].setNextTarget(super().pickRandomTarget(room, {self._games.getTarget()}))
     
     def update(self):
-        super().update(game_manager.pickNextTarget)
+        super().update(self.pickNextTarget)
+
+    def pickNextTarget(self, game, score, other_target):
+        if(score):
+            game.addPoint()
+        exceptions = []
+        exceptions.append(game.getTarget())
+        exceptions.append(game.getNextTarget())
+        game.resetTarget(game.getTarget())
+        game.resetTarget(game.getNextTarget())
+        game.setTarget(super().pickRandomTarget(game.getRoom(), exceptions))
+        exceptions.append(game.getTarget())
+        game.setNextTarget(super().pickRandomTarget(game.getRoom(), exceptions))
+        game.colorTarget(game.color_primary, game.getNextTarget())
+        game.resetCounter()
+
+
+
 
 if __name__ == '__main__':
     print('Running. Press CTRL-C to exit.')
