002d5a21a01780c39e8b1ccd13f36497f9124a78
Brennan Lagasse <brennan.lagasse@gmail.com>
2022-12-28 12:23:33 -0500

    updated game to fix errors in basic version

--- Raspberry Pi Code/Abstract Implementation/games/1p_shoulder_check_advanced.py
+++ Raspberry Pi Code/Abstract Implementation/games/1p_shoulder_check_advanced.py
@@ -2,14 +2,14 @@
 # Description:
 # Targets 4-6 and 2 and 8 light up
 # Player must hit the front target (4-6) that matches the light in the back (2 or 8) that turns on in the same color
-# Only 2 OR 8 turns on
+# 2 AND 8 turn on
 
 from game_manager import GameManager
 from random import randint
 
 NUM_PLAYERS = 1
 
-class ShoulderCheckAdvancedGame(GameManager):
+class ShoulderCheckBasicGame(GameManager):
     """Standard Game"""
     def __init__(self):
         super().__init__(NUM_PLAYERS)
@@ -29,42 +29,73 @@ class ShoulderCheckAdvancedGame(GameManager):
                     game.colorTarget(color, i)
 
                     if i == game.getTarget():
-                        # Make a list of back targets
-                        back_targets = [1, 7]
+                        # Random seed for back target that matches
+                        randomSeed = randint(0,1)
 
-                        # Turn on either target 2 (1) or 8 (7) to match
-                        game.colorTarget(color, back_targets.pop(randint(0,1)))
+                        # Turn on either target 1 (0) or 8 (7) to match
+                        game.colorTarget(color, 1 + 6*randomSeed)
 
-                        # Color the other target something left
-                        game.colorTarget(colors.pop(randint(0, len(colors) - 1)), back_targets[0])
+                        # Color the remaining back target with a remaining color
+                        game.colorTarget(colors.pop(randint(0, len(colors) - 1)), 1 + 6*((randomSeed + 1)%2))
 
         
     def update(self):
-        super().update(self.checkTargets, self.pickNextTarget, self.standardLightUpdate)
+        super().update(self.checkTargets, self.pickNextTarget, self.lightUpdate)
 
-    def randomColors(self, game):
-        colors = [game.ORANGE, game.YELLOW, game.GREEN, game.BLUE, game.PURPLE, game.PINK, game.WHITE]
+    def pickNextTarget(self, game, score, other_game):
+        """Pick the next target and set new game state. Update score"""
+        if(score):
+            self.addPoints(game)
+
+        # Reset all lights to be careful
+        for i in range(game.getRoom() * 8, game.getRoom() * 8 + 8):
+            game.resetTarget(i)
+
+        # Pick the next target
+        game.setTarget(super().pickRandomTarget(game.getRoom(), [0, 1, 2, 6, 7]))
 
         # Color front targets randomly 4-6 (targets[3:5]) 
         # and color one back target the same color 2 or 8 (targets[1] or targets[7])
+        colors = [game.ORANGE, game.YELLOW, game.GREEN, game.BLUE, game.PURPLE, game.PINK, game.WHITE]
+
         for i in range(game.getRoom() * 8 + 3, game.getRoom() * 8 + 6):
             color = colors.pop(randint(0, len(colors) - 1))
             game.colorTarget(color, i)
 
             if i == game.getTarget():
-                # Make a list of back targets
-                back_targets = [1, 7]
+                # Random seed for back target that matches
+                randomSeed = randint(0,1)
 
-                # Turn on either target 2 (1) or 8 (7) to match
-                game.colorTarget(color, back_targets.pop(randint(0,1)))
+                # Turn on either target 1 (0) or 8 (7) to match
+                game.colorTarget(color, 1 + 6*randomSeed)
 
-                # Color the other target something left
-                game.colorTarget(colors.pop(randint(0, len(colors) - 1)), back_targets[0])
+                # Color the remaining back target with a remaining color
+                game.colorTarget(colors.pop(randint(0, len(colors) - 1)), 1 + 6*((randomSeed + 1)%2))
+
+        # Reset counter
+        game.resetCounter()
+
+    def lightUpdate(self, newTargetPicker):
+        """Does countdown for target in each game and resets when timer ends. Override for other"""
+        for room in self._games:
+            game = room[0]
+
+            # Update lights
+            game.updateLightsCountdown()
+
+            # Update front targets
+            for i in range(3,6):
+                game.updateLightsCountdownAlt(game.getRoom()*8+i)
+
+            # Update target if all lights are out
+            if game.checkCountdownEnded():
+                newTargetPicker(game, False, self._games[game.getRoom()][(i+1) % 2])
+
 
 if __name__ == '__main__':
     print('Running. Press CTRL-C to exit.')
 
-    game_manager = ShoulderCheckAdvancedGame()
+    game_manager = ShoulderCheckBasicGame()
 
     try:
         while not game_manager.timeExpired():
