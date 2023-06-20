17be3d3e81214bb37065ab0e5b66bb40791d87f7
Brennan Lagasse <brennan.lagasse@gmail.com>
2023-06-20 19:51:58 -0400

    changed usb connections

--- Raspberry Pi Code/Abstract Implementation/game_manager_app.py
+++ Raspberry Pi Code/Abstract Implementation/game_manager_app.py
@@ -40,7 +40,7 @@ class GameManager:
         # Create and store serial access
         self.serial_connections = []
         self.serial_connections.append(serial.Serial("/dev/ttyUSB0", 9600, timeout=5))
-        self.serial_connections.append(serial.Serial("/dev/ttyUSB1", 9600, timeout=5))
+        # self.serial_connections.append(serial.Serial("/dev/ttyUSB1", 9600, timeout=5))
         # self.serial_connections.append(serial.Serial("/dev/ttyUSB2", 9600, timeout=5))
         # self.serial_connections.append(serial.Serial("/dev/ttyUSB3", 9600, timeout=5))
 
